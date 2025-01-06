f6bb52bd5e9cf58555a8f8ad1e411e38c7ed2274
Okta-jpg <oktavianuschn@gmail.com>
2025-01-07 05:57:28 +0700

    Villora App

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,110 @@
+import { FeaturedCard, Card } from "@/app/components/Cards";
+import Search from "@/app/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { Text, View , Image, TouchableOpacity, FlatList, Button, ActivityIndicator} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "@/app/lib/global-provider";
+import { useAppwrite } from "@/app/lib/useAppwrite";
+import { getLatestProperties, getProperties } from "@/app/lib/appwrite";
+import { useEffect } from "react";
+import seed from "@/app/lib/seed";
+import NoResults from "@/app/components/NoResults";
+import Filters from "@/app/components/filter";
+
+export default function Index() {
+
+  const { user } =useGlobalContext();
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+  
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({
+    fn: getLatestProperties
+  });
+
+  const { data: properties, loading, refetch} = useAppwrite({
+    fn: getProperties,
+    params: { query: params.query!, filter: params.filter!, limit: 6 }, skip: true,
+  })
+
+    useEffect(()=> {
+        refetch({
+          filter: params.filter!,
+          query: params.query!,
+          limit: 6
+        })
+    },[params.filter, params.query])
+
+    const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+  
+
+
+  return (
+   <SafeAreaView className="bg-white h-full">
+
+      <FlatList data={properties} 
+      renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)} /> }
+      keyExtractor={(item) => item.$id}
+      numColumns={2}
+      contentContainerClassName="pb-32"
+      columnWrapperClassName="flex gap-5 px-5"
+      showsVerticalScrollIndicator={false}
+      ListEmptyComponent={
+        loading ? (
+          <ActivityIndicator size="large" className="text-primary-300 mt-5"/> ) : <NoResults/>
+      }
+      ListHeaderComponent={
+        <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={{ uri: user?.avatar}} className="size-12 rounded-full"/>
+              <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                  <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6"/>
+        </View>
+
+    <Search/>
+
+    <View className="my-5">
+      <View className="flex flex-row items-center justify-between">
+        <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+        <TouchableOpacity>
+          <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+        </TouchableOpacity>
+      </View>
+
+       {latestPropertiesLoading ? 
+        <ActivityIndicator size="large" className="text-primary-300"/> :
+          !latestProperties || latestProperties.length === 0 ? <NoResults/> :
+      <FlatList data={latestProperties} 
+      renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)}/> }
+      keyExtractor={(item) => item.$id}
+      horizontal
+      bounces={false}
+      showsHorizontalScrollIndicator={false}
+      contentContainerClassName="flex gap-5 mt-5"
+      />}
+
+    </View>
+
+    <View className="flex flex-row items-center justify-between">
+        <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+        <TouchableOpacity>
+          <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+        </TouchableOpacity>
+        </View>
+
+        <Filters/>
+      </View>
+
+  
+      }
+      />
+
+
+   </SafeAreaView>
+  );
+}
